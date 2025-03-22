ab4b9c1e3d207d9c88423f7b524142903df9ff96
Fares Maaty <fmaaty@std.mans.edu.eg>
2025-03-22 22:20:12 +0200

    TaskWeek3_.NET Backend

--- Program.cs
+++ Program.cs
@@ -1,70 +1,49 @@
-﻿using System.Diagnostics;
-using System.Xml.Linq;
-
-namespace Project1
+﻿namespace Task3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Student student = new Student();
-            PostGraduate pg = new PostGraduate {  ThesisTopic = "AI in Healthcare" };
-            pg.Name = "Fares";
-            pg.Age = 20;
-            pg.Grade = 90;
-            pg.PrintDetails();
-
-            Employee emp = new Employee { Name = "Fares", Gender = 'M', Id = 100, Salary = 1_00_00, Security = SecurityLevel.Developer };
-            Console.WriteLine(emp);
-        }
-    }
-    class Student
-    {
-        public int Age { get; set; }
-        public int Grade { get; set; }
-        public string Name { get; set; }
-        public void PrintDetails()
-        {
-            Console.WriteLine($"MyName is {Name} - Age is {Age} - Grade:{Grade}");
-        }
-    }
-    class PostGraduate : Student
-    {
-        public string ThesisTopic { get; set; }
-        public virtual void PrintDetails()
-        {
-            base.PrintDetails();
-            Console.WriteLine(ThesisTopic);
-        }
-    }
-    class Employee
-    {
-        char gender;
-        public string Name { get; set; }
-        public char Gender
-        {
-            get => gender;
-            set
+            //(1)
+            Swap(10, 20);
+            Swap("fares", "maaty");
+            //(2)
+            Dictionary<string, int> student = new Dictionary<string, int>();
+            student.Add("Mohamed", 90);
+            student.Add("fares"  , 85);
+            student.Add("Ali"    , 75);
+            student.Add("Omar"   , 65);
+            student.Add("Mohanad", 50);
+            DisplayGrade(student);
+            //(3)
+            Queue<string> customerService = new Queue<string>();
+            customerService.Enqueue("Mohamed");
+            customerService.Enqueue("fares"  );
+            customerService.Enqueue("Ali"    );
+            customerService.Enqueue("Omar"   );
+            customerService.Enqueue("Mohanad");
+            Console.WriteLine("CustomerNames:");
+            while (customerService.Count > 0)
             {
-                if (value == 'F' || value == 'M')
-                    gender = value;
-                else
-                    throw new ArgumentException("Gender must be 'M' or 'F'");
+                Console.WriteLine(customerService.Dequeue());
+            }
+
+        }
+        static void Swap<T>(T variable1,T variable2)
+        {
+            Console.WriteLine($"Result Before Swap:variable1={variable1} - variable2={variable2}");
+            T swap = variable1;
+            variable1 = variable2;
+            variable2 = swap;
+            Console.WriteLine($"Result After Swap:variable1={variable1} - variable2={variable2}");
+        }
+
+        static void DisplayGrade(Dictionary<string,int> dic)
+        {
+            for(int i = 0; i < dic.Count; i++)
+            {
+                Console.WriteLine($"StudentName:{dic.Keys.ElementAt(i)} => Grade:{dic.Values.ElementAt(i)}");
             }
         }
-        public int Id { get; set; }
-        public double Salary { get; set; }
-        public SecurityLevel Security { get; set; }
-        public override string ToString()
-        {
-            return String.Format($"Name:{Name} - Gender:{gender} - Id:{Id} - Salary {Salary:c} - SecurityLevel:{Security}");
-        }
-    }
-    enum SecurityLevel
-    {
-        uest,
-        Developer,
-        secretary,
-        DBA
     }
 }
