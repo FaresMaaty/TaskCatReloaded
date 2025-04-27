dd857bc1599fa6eee8b3978bd11810f703f68bf5
Fares Maaty <fmaaty@std.mans.edu.eg>
2025-04-27 20:20:25 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿namespace Project.TaskOfDelegate
+namespace Project.TaskOfDelegate
 {
     delegate int Calculator(int number1, int number2);
-    delegate int Summition(int[] arr);
+    delegate int Summation(int[] arr);
     internal class Program
     {
         static void Main(string[] args)
@@ -10,16 +10,23 @@
             Calculator addition = (int number1, int number2) => number1 + number2;
             Calculator subtraction = (int number1, int number2) => number1 - number2;
             Calculator multiplication = (int number1, int number2) => number1 * number2;
-
             Console.WriteLine(addition(2, 3));
             Console.WriteLine(subtraction(2, 3));
             Console.WriteLine(multiplication(2, 3));
-
-
+            
+            //(2)
+            DataProcessor processor = new DataProcessor();
+            processor.OnDataReceived += (sender, e) =>
+            {
+                Console.WriteLine("Notification: New data has been received!");
+            };
+            Console.WriteLine("Data Processor Simulation:");
+            processor.ReceiveData();
+            Console.WriteLine();
 
 
             //(3)
-            Summition sum = delegate (int[] arr)
+            Summation sum = delegate (int[] arr)
             {
                 int sum = 0;
                 foreach (var number in arr)
@@ -31,4 +38,16 @@
             Console.WriteLine(sum(new int[] { 2, 5, 3, 3, 7 }));
         }
     }
+
+    public class DataProcessor
+    {
+        public event EventHandler OnDataReceived;
+
+        public void ReceiveData()
+        {
+            Console.WriteLine("[Simulating data reception]");
+            OnDataReceived?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
 }
